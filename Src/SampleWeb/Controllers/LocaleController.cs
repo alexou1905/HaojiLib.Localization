99de381a685ca6dc32c9965d015d059c961b6c5b
Alex <alexou81@live.com>
2016-02-21 17:06:11 +0800

    sample web

---
+++ Src/SampleWeb/Controllers/LocaleController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using HaojiLib.Localization;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SampleWeb.Controllers
+{
+    public class LocaleController : Controller
+    {
+        private ILocalizationService locSvc;
+
+        public LocaleController(ILocalizationService locSvc)
+        {
+            this.locSvc = locSvc;
+        }
+        // GET: /<controller>/
+        public IActionResult Change(String culture)
+        {
+            locSvc.SetCulture(culture);
+            Response.Redirect(Request.Headers["Referer"]);
+            return Content("OK");
+        }
+    }
+}
