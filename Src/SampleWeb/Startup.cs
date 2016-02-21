4e691e67073ccedd0e5766e05aac0508f37033a9
Alex <alexou81@live.com>
2016-02-21 16:47:05 +0800

    demo web

--- Src/SampleWeb/Startup.cs
+++ Src/SampleWeb/Startup.cs
@@ -16,6 +16,7 @@ namespace SampleWeb
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services )
         {
+            services.AddMvc();
             services.AddSingleton<ILocalizationService, LocalizationService>();
         }
 
@@ -23,11 +24,8 @@ namespace SampleWeb
         public void Configure(IApplicationBuilder app)
         {
             app.UseIISPlatformHandler();
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
 
         // Entry point for the application.
